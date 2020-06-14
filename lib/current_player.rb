975d34615ea1dda7b61fee48a5795e41da8a691c
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-14 22:17:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+counter = 0 
 
-def turn_count
+def turn_count(board)
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1 
+    end
   
 end
 
